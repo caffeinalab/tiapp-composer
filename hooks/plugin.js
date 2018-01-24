16f3e6266a1307bb971609f238c2f64881c7a448
Jei <j.jonus.j@gmail.com>
2018-01-24 18:26:48 +0100

    First working version

--- hooks/plugin.js
+++ hooks/plugin.js
@@ -1,36 +1,80 @@
 /*
 * @Author: andrea.jonus
 * @Date:   2018-01-24 10:57:19
-* @Last Modified by:   andrea.jonus
-* @Last Modified time: 2018-01-24 11:37:07
+* @Last Modified by:   Jei
+* @Last Modified time: 2018-01-24 18:24:46
 */
 
 console.log("Running tiapp-composer-plugin...");
 
 exports.cliVersion = '>=3.X';
+exports.version = '1.0.0';
 
-let projectDir = null;
-const TIAPP_TEMPLATE = 'tiapp.tpl';
-const OUTFILE = 'tiapp.xml';
+const projectDir = process.cwd();
+const TIAPP_TEMPLATE = projectDir + '/tiapp.tpl';
+const OUTFILE = projectDir + '/tiapp.xml';
+let tiappEnv = null;
 
 async function compose(env, tplfile, outfile) {
-  if (!env) throw new Error('Missing environment');
+  const fs = require('fs');
   const { app } = env;
 
-  const tpl = await fs.readFile(tplfile);
-  const composed = eval('`' + tpl + '`');
+  return new Promise((resolve, reject) => {
+    fs.readFile(tplfile, (err, tpl) => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
-  return fs.writeFile(outfile, composed);
+      const composed = eval('`' + tpl + '`');
+
+      fs.writeFile(outfile, composed, (err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        resolve();
+      })
+    });
+  });
 }
 
 exports.init = function (logger, config, cli, appc) {
-  const fs = require('fs');
-  const tiappCfg = require('tiapp-cfg');
+  cli.on("cli:go", async function (build, finished) {
+    let { tiappenv } = cli.globalContext.argv;
 
-  cli.on("build.pre.construct", function (build, finished) {
-    projectDir = build.projectDir;
+    if (tiappenv == null) {
+      logger.warn('--tiappenv flag not set, defaulting to "development"');
+      tiappenv = "development";
+    }
 
-    // TODO
+    let tiappCfg = null;
+
+    try {
+      tiappCfg = require(projectDir + '/tiapp-cfg');
+    } catch(err) {
+      logger.warn("Couldn't find a tiapp-cfg.json file:", err);
+      logger.warn('Skipping tiapp.xml composing.');
+      finished();
+      return;
+    }
+
+    if (tiappCfg[tiappenv] == null) {
+      logger.warn(`Couldn't find the environment "${tiappenv}" in the tiapp-cfg.json file.`);
+      logger.warn('Skipping tiapp.xml composing.');
+      finished();
+      return;
+    }
+
+    try {
+      await compose(tiappCfg[tiappenv], TIAPP_TEMPLATE, OUTFILE);
+
+      logger.info('Successfully wrote tiapp.xml');
+    } catch(err) {
+      logger.warn("Couldn't write the new tiapp.xml file:", err);
+      logger.warn('Skipping tiapp.xml composing.');
+    }
 
     finished();
   });
