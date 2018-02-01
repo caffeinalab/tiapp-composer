56a05a7ebb7ece5bc7c3c9e80c74ca8d2608bfa0
Jei <j.jonus.j@gmail.com>
2018-02-01 14:51:25 +0100

    Added warning for missing tiapp.xml line in gitignore.

--- hooks/plugin.js
+++ hooks/plugin.js
@@ -2,18 +2,73 @@
 * @Author: andrea.jonus
 * @Date:   2018-01-24 10:57:19
 * @Last Modified by:   Jei
-* @Last Modified time: 2018-01-25 12:30:25
+* @Last Modified time: 2018-02-01 14:48:25
 */
 
-console.log("Running tiapp-composer-plugin...");
+const TAG = 'tiapp-composer-plugin';
+console.log(`Running ${TAG}...`);
 
 exports.cliVersion = '>=3.X';
 exports.version = '1.0.0';
 
 const projectDir = process.cwd();
 const TIAPP_TEMPLATE = projectDir + '/tiapp.tpl';
-const OUTFILE = projectDir + '/tiapp.xml';
+const TIAPP_FILE = 'tiapp.xml';
+const OUTFILE = projectDir + '/' + TIAPP_FILE;
+const GIT = projectDir + '/.git';
+const GITIGNORE = projectDir + '/.gitignore';
 let tiappEnv = null;
+
+function checkGit(lg) {
+  const logger = lg != null ? lg : console;
+  const fs = require('fs');
+
+  return new Promise((resolve, reject) => {
+    fs.stat(GIT, (err, stats) => {
+      if (err) {
+        if (err.code != "ENOENT") {
+          logger.warn(`${TAG}: Couldn't access .git directory:`, err);
+        }
+
+        reject();
+        return;
+      }
+
+      if (stats.isDirectory()) {
+        resolve(logger);
+      } else {
+        reject();
+      }
+    });
+  });
+}
+
+function checkGitIgnore(lg) {
+  const logger = lg != null ? lg : console;
+  const fs = require('fs');
+  const readline = require('readline');
+
+  let found = false;
+
+  const lineReader = require('readline').createInterface({
+    input: fs.createReadStream(GITIGNORE),
+  });
+
+  lineReader.on('line', (line) => {
+    const trimmed = line.trim();
+    if (trimmed == TIAPP_FILE || trimmed == ('/' + TIAPP_FILE)) {
+      found = true;
+
+      lineReader.close();
+    }
+  });
+
+  lineReader.on('close', () => {
+    if (!found) {
+      logger.warn(`${TAG}: This plugin will overwrite your tiapp. You should add "${TIAPP_FILE}" to your .gitignore`);
+    }
+  });
+}
 
 function compose(env, tplfile, outfile) {
   const fs = require('fs');
@@ -44,7 +99,7 @@ function checkAndCompose(cli, logger, finished) {
   let { tiappenv } = cli.globalContext.argv;
 
   if (tiappenv == null) {
-    logger.warn('--tiappenv flag not set, defaulting to "development"');
+    logger.warn(`${TAG}: --tiappenv flag not set, defaulting to "development"`);
     tiappenv = "development";
   }
 
@@ -53,26 +108,26 @@ function checkAndCompose(cli, logger, finished) {
   try {
     tiappCfg = require(projectDir + '/tiapp-cfg');
   } catch(err) {
-    logger.warn("Couldn't find a tiapp-cfg.json file:", err);
-    logger.warn('Skipping tiapp.xml composing.');
+    logger.warn(`${TAG}: Couldn't find a tiapp-cfg.json file:`, err);
+    logger.warn(`${TAG}: Skipping tiapp.xml composing.`);
     finished();
     return;
   }
 
   if (tiappCfg[tiappenv] == null) {
-    logger.warn(`Couldn't find the environment "${tiappenv}" in the tiapp-cfg.json file.`);
-    logger.warn('Skipping tiapp.xml composing.');
+    logger.warn(`${TAG}: Couldn't find the environment "${tiappenv}" in the tiapp-cfg.json file.`);
+    logger.warn(`${TAG} Skipping tiapp.xml composing.`);
     finished();
     return;
   }
 
   compose(tiappCfg[tiappenv], TIAPP_TEMPLATE, OUTFILE)
   .then(() => {
-    logger.info('Successfully wrote tiapp.xml');
+    logger.info(`${TAG}: Successfully wrote tiapp.xml`);
   })
   .catch((err) => {
-    logger.warn("Couldn't write the new tiapp.xml file:", err);
-    logger.warn('Skipping tiapp.xml composing.');
+    logger.warn(`${TAG}: Couldn't write the new tiapp.xml file:`, err);
+    logger.warn(`${TAG}: Skipping tiapp.xml composing.`);
   })
   .then(() => {
     finished();
@@ -80,7 +135,12 @@ function checkAndCompose(cli, logger, finished) {
 }
 
 exports.init = function (logger, config, cli, appc) {
+  checkGit(logger)
+  .then(checkGitIgnore)
+  .catch(() => {
+    logger.log(`${TAG}: Git not detected.`);
+  });
+
   cli.on("build.config", (build, finished) => checkAndCompose(cli, logger, finished));
   cli.on("clean.config", (build, finished) => checkAndCompose(cli, logger, finished));
 };
-
